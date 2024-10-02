2a7a7ea71cf3ea840075dd715f27b2ddc875e6da
Ahmed Fahmy <korabika552@gmail.com>
2024-10-02 14:34:32 +0300

    init

---
+++ Assets/__Scripts/Tools/LookAtOnDrawGizmos.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class LookAtOnDrawGizmos : MonoBehaviour
+{
+    [Header("Inscribed")]
+    [Tooltip("The Transform this should look at")]
+    public Transform lookAt;
+
+    private void OnDrawGizmos()
+    {
+        if (lookAt != null)
+        {
+            transform.LookAt(lookAt);
+        }
+    }
+}
